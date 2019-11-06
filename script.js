6925ae5174064cc2c9d756d9b41ba206cdcbca68
Yol-B <yoltorres24@gmail.com>
2019-11-06 18:54:33 +0800

    work on backend

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $("#go").click(function () {
+    
+    var firstName = $('#name').val();
+    var lastName = $('#subject').val();
+    var age = $('#Age').val();
+    var gender = $('#courseYear').val();
+    var address = $('#Address').val();
+
+    var requestUrl = 'http://localhost:8080/visit/'+studentName;
+    var data = JSON.stringify({ "FirstName": firstName, "LastName": lastName, "Age": age, "Gender": gender,"address":address })
+    apiRequest(requestUrl, data)
+  });
+  function apiRequest(apiurl,apidata = true) {
+    $.ajax({
+      url: apiurl,
+      data: apidata,
+      type: "POST",
+      success: function (result) {
+        swal({
+          title: "Success!",
+          text: result + " has been added!",
+          icon: "success",
+          button: "OK",
+        });
+      }
+    });
+  }
+});
