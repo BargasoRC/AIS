9eb95fa17d055120363f97933a89bcd6bcaefeb8
Yol-B <yoltorres24@gmail.com>
2019-11-06 18:27:16 +0800

    work on backend

--- router.js
+++ router.js
@@ -1,23 +1,24 @@
 var url = require('url');
-var mod= require('./module');
-var Enroll = require('./enroll');
 var express = require('express');
 var app = express();
+var express = require('express');
 var port = 8080;
 var path = require('path');
 var http = require('http').Server(app);
+
+app.use(express.static('public'));
 
 app.all('/', function(req, res){
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.all('/enroll', function(req, res){
-    Enroll.enroll(req,res);
-});
+// app.all('/enroll', function(req, res){
+//     Enroll.enroll(req,res);
+// });
 
-app.all('/class/:subject', function(req, res){
-    mod.csvtabular(req,res);
-});
+// app.all('/class/:subject', function(req, res){
+//     mod.csvtabular(req,res);
+// });
 
 http.listen(port, function(){
     console.log('listening to port: '+port);
